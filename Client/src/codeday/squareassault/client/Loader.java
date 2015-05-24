9b4e30aed008480f08379681a984750e68a4837a
Colby Skeggs <skeggsc@catlin.edu>
2015-05-23 18:18:25 -0700

    Made loader cache properly.

--- Client/src/codeday/squareassault/client/Loader.java
+++ Client/src/codeday/squareassault/client/Loader.java
@@ -2,13 +2,21 @@ package codeday.squareassault.client;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 public class Loader {
-	public static BufferedImage load(String name) {
+	private static final HashMap<String, BufferedImage> loaded = new HashMap<>();
+
+	public static synchronized BufferedImage load(String name) {
 		try {
-			return ImageIO.read(Loader.class.getResourceAsStream("/codeday/squareassault/resources/" + name + ".png"));
+			if (loaded.containsKey(name)) {
+				return loaded.get(name);
+			}
+			BufferedImage out = ImageIO.read(Loader.class.getResourceAsStream("/codeday/squareassault/resources/" + name + ".png"));
+			loaded.put(name, out);
+			return out;
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
